4010f953c5246286348d64fc5f560e3c8f454baf
NomiH <138275265+NomiH04@users.noreply.github.com>
2025-01-14 13:59:15 -0600

    Business logic layer (capa logica de negocios)

---
+++ BLL/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Usuario
+    {
+        public string Email { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Password { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public char Estado { get; set; }
+        public string Rol { get; set; }
+        public string Foto { get; set; }
+
+        // Metodo para confirmar clave
+        public bool ConfirmarPassword(string ps)
+        {
+            bool autorizado = false;
+
+            if (string.IsNullOrEmpty(ps))
+            {
+                return false;
+            }
+            else
+            {
+                if (Password.Equals(ps))
+                {
+                    autorizado = true;
+                }
+            }
+            return autorizado;
+        }//Fin metodo confirmarPass
+    }//Fin clase
+}//Fin Name space

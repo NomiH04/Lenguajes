ce87611b4a5c14ed6492a7c1f2c63372eee03640
NomiH <138275265+NomiH04@users.noreply.github.com>
2025-01-21 13:56:44 -0600

    MV

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Zaguate.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zaguate.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private static List<Usuario> usuarios = null;
+        public UsuarioController() {
+            usuarios = GetUsuarios();
+        }
+        public  List<Usuario> GetUsuarios()
+        {
+            return new List<Usuario>{
+                new Usuario{ IdUsuario = 1, Nombre = "Cazuario", Email ="Keli@suHermana.com", Clave = "LaSopaMaggie", Estado = true},
+
+                new Usuario { IdUsuario = 2, Nombre = "Zapato", Email = "elpie@desuHermana.com", Clave = "elzapato", Estado = false}
+            };
+                
+        }
+        public IActionResult Index()
+        {
+            return View(usuarios);
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Zaguate.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
